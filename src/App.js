6aa6b397a3392a795a0f7890d1fe285f6319d57f
siwarbenrhouma <benrhoumamohamed752@gmail.com>
2022-08-18 15:49:56 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import react,{component} from 'react'
+import imgc from "./277081215_1629829970699664_8164463741762050939_n.jpg"
 
-function App() {
+class App extends react.Component {
+  
+  state={
+    person:{
+      FullName:"Siwar Ben Rhouma",
+      bio:"It is better to fail in originality than to succeed in imitation.",
+      imgSrc:imgc,
+      profession:"computer science engineer",
+    },
+    boll:false
+
+  }
+  changeColor = () => {
+    this.setState({boll:!this.state.boll})
+  }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <button onClick={this.changeColor}>click here</button>
+      {this.state.boll?
+        <div>
+      <img src={this.state.person.imgSrc}/>
+      <h1> my name is {this.state.person.FullName}<br/>
+      i work as {this.state.person.profession}<br/>
+      my favourite quote is {this.state.person.bio} </h1>
+      </div>:""
+  }
     </div>
-  );
+  )
 }
 
+}
 export default App;
